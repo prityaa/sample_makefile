dafa93b13666067e90bf4c13f6aa7d78b634463d
prityaa <pritam.sutar@ril.com>
2020-07-25 12:13:26 +0530

    debug  added in header

--- include/hello.h
+++ include/hello.h
@@ -4,3 +4,6 @@ example include file
 
 void myPrintHelloMake(void);
 
+#define dbg(fmt, ...) \
+        do { if (1) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+                                __LINE__, __func__, ##__VA_ARGS__); } while (0)
--- src/hello.c
+++ src/hello.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <hello.h>
 
 int main() {
   // call a function in another file
   myPrintHelloMake();
-
+  dbg("main\n");
   return(0);
 }
 
